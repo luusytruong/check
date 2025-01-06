5fbf66541761817d680162eb688d7a8a7d4dd788
luusytruong <lst27062004@gmail.com>
2025-01-06 18:47:51 +0700

    update: toggle on/off, select subjects, test

--- content/content.js
+++ content/content.js
@@ -11,15 +11,24 @@ const styleS = `
   padding: 4px 12px 4px 4px; 
   border-radius: 50px;
 `;
+const styleE = `
+  color: #F8312F; 
+  background-color: #3C3D51; 
+  padding: 4px 12px 4px 4px; 
+  border-radius: 50px;
+`;
+let subjectIndex = 0;
+let testIndex = 0;
+
 //func interval
 function interval() {
   const interval = 1000;
   function checkNav() {
     const intervalId = setInterval(() => {
       nav = document.querySelector(".ictu-page-test__test-panel__single-nav");
-      console.log("check");
+      // console.log("check");
       if (nav) {
-        console.log("tim duoc nav");
+        // console.log("tim duoc nav");
         removeDisabled();
         clearInterval(intervalId);
       }
@@ -27,11 +36,11 @@ function interval() {
   }
   //check header a
   function checkHeaderA() {
-    console.log("%c🟡 wait check logo ...", styleW);
+    // console.log("%c🟡 wait check logo ...", styleW);
     const intervalId = setInterval(() => {
       const a = document.querySelector(".m-header a");
       if (a) {
-        console.log("%c🟢 logo exist", styleS);
+        // console.log("%c🟢 logo exist", styleS);
         //call
         const btnClass = document.querySelector(
           ".nav >:nth-child(2) >:nth-child(3) a"
@@ -40,26 +49,40 @@ function interval() {
           clearInterval(intervalId);
           btnClass.click();
           const intervalId2 = setInterval(() => {
+            // console.log("%c🟡 check subject ...", styleW);
             const btnSubject = document.querySelector(
-              ".classes-container >:nth-child(3) a"
+              `.classes-container >:nth-child(${subjectIndex}) a`
             );
             if (btnSubject) {
+              // console.log("%c🟢 subject exist", styleS);
               btnSubject.click();
               clearInterval(intervalId2);
               const intervalId3 = setInterval(() => {
-                const btnExample = document.querySelector(
-                  ".activity-left >:nth-child(5) >div"
+                // console.log("%c🟡 check test ...", styleW);
+                const btnTest = document.querySelector(
+                  `.activity-left >:nth-child(${testIndex}) >div`
                 );
-                if (btnExample) {
-                  btnExample.click();
+                if (btnTest) {
+                  // console.log("%c🟢 test exist", styleS);
+                  btnTest.click();
                   clearInterval(intervalId3);
+                  let find = 10;
                   const intervalId4 = setInterval(() => {
-                    const btnExampleView = document.querySelector(
-                      ".activity-left >:nth-child(5) >ul>li:nth-child(3) >div"
+                    // console.log("%c🟡 check, can i do it ...", styleW);
+                    const btnTestView = document.querySelector(
+                      `.activity-left >:nth-child(${testIndex}) >ul>li:nth-child(3) >div`
                     );
-                    if (btnExampleView) {
-                      btnExampleView.click();
+                    if (btnTestView) {
+                      // console.log("%c🟢 let's do it", styleS);
+                      btnTestView.click();
                       clearInterval(intervalId4);
+                    } else {
+                      if (find === 0) {
+                        // console.log("%c🔴 can not do", styleE);
+                        clearInterval(intervalId4);
+                        return;
+                      }
+                      find--;
                     }
                   }, interval);
                 }
@@ -73,11 +96,12 @@ function interval() {
   //call
   checkHeaderA();
 }
+
 //remove disabled
 function removeDisabled() {
   const btnSubmits = document.querySelectorAll(".ictu-button--disabled");
   if (btnSubmits.length) {
-    console.log("tim duoc 2 nut disabled");
+    // console.log("tim duoc 2 nut disabled");
     btnSubmits.forEach((btnSubmit) => {
       if (btnSubmit.classList.contains("ictu-button--disabled")) {
         btnSubmit.classList.remove("ictu-button--disabled");
@@ -87,11 +111,12 @@ function removeDisabled() {
     });
   }
 }
+
 //add event listener
 function addEventBtnNext() {
   const btnNext = nav.querySelector("button");
   if (btnNext) {
-    console.log("tim duoc nut next");
+    // console.log("tim duoc nut next");
     btnNext.addEventListener("click", () => {
       setTimeout(() => {
         btnNext.classList.remove("ictu-button--disabled");
@@ -100,5 +125,29 @@ function addEventBtnNext() {
     });
   }
 }
+
+//get data from storage
+function getFromStorage() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get(["check"], function (result) {
+      if (chrome.runtime.lastError) {
+        reject(chrome.runtime.lastError);
+      } else {
+        resolve(result.check);
+      }
+    });
+  });
+}
+
+//load data
+async function loading() {
+  const checkObj = await getFromStorage();
+  if (checkObj && checkObj.state === true) {
+    // console.log("%c🟢 extension on", styleS);
+    subjectIndex = checkObj.subject;
+    testIndex = checkObj.test + 1;
+    interval();
+  }
+}
 //start
-interval();
+loading();
---
+++ popup/script.js
@@ -0,0 +1,77 @@
+let objCheck = null;
+const toggle = document.getElementById("toggle-state");
+const subjectElem = document.getElementById("subject");
+const testElem = document.getElementById("test");
+const notifyElem = document.getElementById("notify");
+const btnSave = document.getElementById("save");
+
+//get data from storage
+function getFromStorage() {
+  try {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get(["check"], function (result) {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(result.check);
+        }
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//save data to storage
+function saveToStorage(data) {
+  if (!chrome || !chrome.storage) {
+    return false;
+  }
+  try {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.set({ check: data }, function () {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  } catch (e) {
+    alert(e);
+  }
+}
+
+//notify
+function notify(state, content) {
+  notifyElem.className = state;
+  notifyElem.innerText = content;
+}
+
+//listener event
+btnSave.addEventListener("click", () => {
+  const checkObj = {
+    state: toggle.checked,
+    subject: parseInt(subjectElem.value),
+    test: parseInt(testElem.value),
+  };
+  if (saveToStorage(checkObj)) {
+    notify("successful", "Save data successful");
+  } else {
+    notify("error", "Save data error");
+  }
+});
+
+//load data
+async function loading() {
+  if (chrome && chrome.storage) {
+    objCheck = await getFromStorage();
+    if (objCheck) {
+      toggle.checked = objCheck.state;
+      subjectElem.value = objCheck.subject || 1;
+      testElem.value = objCheck.test || 1;
+    }
+  }
+}
+//start
+loading();
